2ae070747856da36ef481fc4aaa80a3aa3f33374
Viktor Yershov <viktor@sacredgarden.love>
2023-03-29 17:12:22 +0800

    Populate partner_invite_code when user is created

--- sacred_garden/models.py
+++ sacred_garden/models.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 from .managers import CustomUserManager
+
+import random
+import string
 
 
 class User(AbstractUser):
@@ -19,3 +24,18 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+    def populate_partner_invite_code(self):
+        self.partner_invite_code = get_new_invite_code()
+
+
+def get_new_invite_code(n=6):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, n=n))
+
+
+@receiver(pre_save, sender=User)
+def populate_partner_invite_code_for_new_user(sender, instance, update_fields, **kwargs):
+
+    # When user is created
+    if instance.pk is None and instance.partner_invite_code is None:
+        instance.populate_partner_invite_code()
--- sacred_garden/tests.py
+++ sacred_garden/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
-# Create your tests here.
+from sacred_garden import models
+
+from unittest import mock
+
+
+class UserCreateTestCase(TestCase):
+
+    @mock.patch('sacred_garden.models.get_new_invite_code')
+    def test_invite_code_is_populated(self, mocked_get_new_invite_code):
+        mocked_get_new_invite_code.return_value = 'QWE123'
+
+        user = models.User()
+        user.save()
+
+        user = models.User.objects.get(pk=user.pk)
+        self.assertEqual(user.partner_invite_code, 'QWE123')
+
+
+class UserUpdateTestCase(TestCase):
+    @mock.patch('sacred_garden.models.get_new_invite_code')
+    def test_invite_code_is_not_updatecd(self, mocked_get_new_invite_code):
+        mocked_get_new_invite_code.return_value = 'QWE123'
+
+        user = models.User(partner_invite_code='ALPHA5')
+        user.save()
+
+        user = models.User.objects.get(pk=user.pk)
+        user.save()
+
+        user = models.User.objects.get(pk=user.pk)
+        self.assertEqual(user.partner_invite_code, 'ALPHA5')
