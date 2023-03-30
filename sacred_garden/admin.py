0b0a59c8dc71d88a39dac2117c61fe197c946b4d
Viktor Yershov <viktor@sacredgarden.love>
2023-03-30 17:18:52 +0800

    Implement /home view + improved admin

--- sacred_garden/admin.py
+++ sacred_garden/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 from .forms import UserCreationForm, UserChangeForm
 from .models import User
 
 
-class UserAdmin(UserAdmin):
+class UserAdmin(DjangoUserAdmin):
     add_form = UserCreationForm
     form = UserChangeForm
     model = User
     list_display = ("email", "is_staff", "is_active",)
     list_filter = ("email", "is_staff", "is_active",)
     fieldsets = (
-        (None, {"fields": ("email", "password", "partner_user", "partner_invite_code")}),
+        (None, {"fields": ("email", "first_name", "password",
+                           "partner_user", "partner_name", "partner_invite_code")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
     )
     add_fieldsets = (
--- sacred_garden/views.py
+++ sacred_garden/views.py
@@ -1,7 +1,28 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from rest_framework import serializers
+
+from sacred_garden import models
+
+
+class PartnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = ['id', 'first_name']
+
+
+class HomeSerializer(serializers.ModelSerializer):
+
+    partner_user = PartnerSerializer()
+
+    class Meta:
+        model = models.User
+        fields = ['id', 'first_name',
+                  'partner_user', 'partner_name', 'partner_invite_code']
+
 
 @api_view()
 def home(request):
-    return Response({"message": "home!"})
+    serializer = HomeSerializer(request.user)
+    return Response(serializer.data)
