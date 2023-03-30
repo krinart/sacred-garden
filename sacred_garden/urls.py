cdcfbf05660de65113841ff3f5477bb173a0eeaf
Viktor Yershov <viktor@sacredgarden.love>
2023-03-30 18:38:20 +0800

    Refactor url

--- sacred_garden/urls.py
+++ sacred_garden/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path, include
+from django.urls import path
 
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+from rest_framework import routers
 
 from sacred_garden import views
 
-router = routers.DefaultRouter()
-router.register(r'home', views.home)
+router = routers.SimpleRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('api/v1/', include(router.urls)),
+    path('api-token-auth/', obtain_jwt_token),
+    path('api-token-refresh/', refresh_jwt_token),
 ]
+
+
+urlpatterns += router.urls
