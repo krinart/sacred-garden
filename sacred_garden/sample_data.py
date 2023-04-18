b8a452c5d06715ccac493bbb68eec78785d385f2
Viktor Yershov <viktor@sacredgarden.love>
2023-04-18 16:15:30 -0700

    Improved sample data

--- sacred_garden/sample_data.py
+++ sacred_garden/sample_data.py
@@ -77,7 +77,7 @@ def _create_eneed_state(eneed, partner, status, trend, text, text_appreciation, 
         partner_user=partner,
     )
 
-    state.created_at = datetime.now() + timedelta(days=day-30)
+    state.created_at = datetime.now() + timedelta(days=day-60)
     state.save()
 
 
@@ -89,60 +89,60 @@ def create_self_emotional_need_states(eneed_user, sample_user):
         "I love you very much and very glad that I have you in my life", 1)
     _create_eneed_state(
         eneed_user, sample_user, -10, 1,
-        "It is important for me to have a connection in a form of physical touch with you becase I love you",
+        "It is important for me to have a connection in a form of physical touch with you because I love you",
         "Thank you for being a great partner and for listening to my needs", 3)
     _create_eneed_state(
         eneed_user, sample_user, -10, -1,
-        "",
-        "Thank you for always being there for me.", 4)
+        "In the past several days I wish I could get more physical touch from you",
+        "Thank you for surprising me with breakfast in bed this morning. You always know how to make my day.", 8)
     _create_eneed_state(
         eneed_user, sample_user, -10, 1,
-        "",
-        "I admire your strength and determination in everything you do.", 5)
+        "I felt much better because you gave me more attention. Thank you!",
+        "I appreciate how you always take the time to listen to me and understand my feelings, even when I'm being irrational.", 11)
     _create_eneed_state(
         eneed_user, sample_user, 0, 1,
-        "",
-        "You make me feel cherished and loved in ways I never thought possible.", 8)
+        "Great work! Please keep it up",
+        "Your dedication to your career is inspiring, and I am so proud of everything you have accomplished.", 14)
     _create_eneed_state(
         eneed_user, sample_user, 0, 0,
-        "",
-        "I am constantly amazed by your intelligence and creativity.", 9)
+        "Good job. I am proud of you",
+        "Thank you for planning such a thoughtful and romantic date for us last night. You always know how to make me feel special.", 18)
     _create_eneed_state(
         eneed_user, sample_user, 0, -1,
-        "",
-        "You have a heart of gold and your kindness never ceases to amaze me.", 10)
+        "It would be great if you could give me a hug every morning after we wake up",
+        "I am grateful for your unwavering support and encouragement as I pursue my own dreams and goals.", 21)
     _create_eneed_state(
         eneed_user, sample_user, 0, 1,
-        "",
-        "Thank you for being patient with me and always putting up with my quirks.", 12)
+        "Thank you for listening to my desires!",
+        "You have a talent for making me laugh even on my toughest days, and I appreciate your sense of humor so much.", 23)
     _create_eneed_state(
         eneed_user, sample_user, 0, 0,
-        "",
-        "", 13)
+        "I am again grateful for what you are doing. Good job :)",
+        "Thank you for always putting our relationship first and making time for us, even when life gets busy.", 26)
     _create_eneed_state(
         eneed_user, sample_user, 0, -1,
-        "",
-        "Thank you for being my best friend and my soulmate.", 15)
+        "I am very grateful for what you already did. Please give me a hug when I am returning from work",
+        "Your kindness and generosity towards others is something I admire so much about you.", 29)
     _create_eneed_state(
         eneed_user, sample_user, -10, -1,
-        "",
-        "I am so grateful for the depth and beauty of our connection.", 20)
+        "I know you are busy yourself, but it is very important for me to have a physical touch from you",
+        "I appreciate how you always go above and beyond to help me, like picking up groceries when I'm too busy to go myself.", 31)
     _create_eneed_state(
         eneed_user, sample_user, -10, -1,
-        "",
-        "I admire your intelligence and how you approach every challenge with grace and wisdom.", 21)
+        "I know it's been hard week. But I am starting to feel unloved without a physical connection",
+        "Your creativity and artistic talent never cease to amaze me, and I feel so lucky to witness it every day.", 36)
     _create_eneed_state(
         eneed_user, sample_user, -20, -1,
-        "",
-        "You have a heart full of compassion and empathy, and it inspires me every day.", 22)
+        "Please pay more attention to this need, it has become a serious problem.",
+        "I appreciate how you always show up for me, whether it's to support me through a tough time or to celebrate a happy moment.", 42)
     _create_eneed_state(
         eneed_user, sample_user, -20, 0,
-        "",
-        "Thank you for the little things you do, like making me coffee in the morning or leaving me sweet notes.", 24)
+        "I appreciate all you did, but I still don't feel loved enough.",
+        "I am grateful for the small acts of kindness you do for me every day, like bringing me a cup of tea when I'm feeling stressed.", 46)
     _create_eneed_state(
         eneed_user, sample_user, -20, -1,
-        "",
-        "I appreciate how you always respect my boundaries and support my dreams and goals.", 25)
+        "I start getting thoughts about physical touch with other people. I don't like those thoughts, so I wish you could help me with this. Thank you!",
+        "Your willingness to take responsibility for your mistakes and work to grow from them is something that I respect so much about you.", 50)
 
 
 def create_partner_emotional_need_states(eneed_sample_user, user):
@@ -154,27 +154,27 @@ def create_partner_emotional_need_states(eneed_sample_user, user):
     _create_eneed_state(
         eneed_sample_user, user, -20, 1,
         "",
-        "You are the most caring and loving person I know.", 2)
+        "I appreciate how you always make an effort to connect with my family and friends and build relationships with them.", 2)
     _create_eneed_state(
         eneed_sample_user, user, -20, -1,
         "",
-        "I appreciate how you always prioritize our relationship and make time for us.", 4)
+        "Your commitment to taking care of your health and well-being inspires me to do the same for myself.", 4)
     _create_eneed_state(
         eneed_sample_user, user, -20, -1,
         "",
-        "Thank you for being my partner in everything and always having my back.", 5)
+        "Your unwavering love and devotion to me make me feel so cherished and valued, and I am so grateful to have you in my life.", 5)
     _create_eneed_state(
         eneed_sample_user, user, -20, 1,
         "",
-        "You bring out the best in me and make me want to be a better person.", 6)
+        "I appreciate how you always show up for me, whether it's to support me through a tough time or to celebrate a happy moment.", 6)
     _create_eneed_state(
         eneed_sample_user, user, -10, 1,
         "",
-        "You have a beautiful soul that radiates love and positivity.", 8)
+        "Thank you for being my partner in adventure and always being willing to try new things with me.", 8)
     _create_eneed_state(
         eneed_sample_user, user, -10, 0,
         "",
-        "Thank you for making me feel cherished and valued, even on my worst days.", 9)
+        "I appreciate how you always go above and beyond to help me, like picking up groceries when I'm too busy to go myself.", 9)
     _create_eneed_state(
         eneed_sample_user, user, -10, 1,
         "",
@@ -182,23 +182,23 @@ def create_partner_emotional_need_states(eneed_sample_user, user):
     _create_eneed_state(
         eneed_sample_user, user, -10, -1,
         "",
-        "You make me feel seen, heard, and understood in ways no one else can.", 12)
+        "You have a talent for making me laugh even on my toughest days, and I appreciate your sense of humor so much.", 12)
     _create_eneed_state(
         eneed_sample_user, user, 0, 1,
         "",
-        "I appreciate your honesty and how you always speak from the heart.", 15)
+        "Thank you for planning such a thoughtful and romantic date for us last night. You always know how to make me feel special.", 15)
     _create_eneed_state(
         eneed_sample_user, user, 0, 0,
         "",
-        "You have a remarkable work ethic and a drive to succeed that I deeply admire.", 16)
+        "Your dedication to your career is inspiring, and I am so proud of everything you have accomplished.", 16)
     _create_eneed_state(
         eneed_sample_user, user, 0, 1,
         "",
-        "Thank you for being my partner in adventure and always being up for trying new things with me.", 18)
+        "Thank you for surprising me with breakfast in bed this morning. You always know how to make my day.", 18)
     _create_eneed_state(
         eneed_sample_user, user, 0, 0,
         "",
-        "You have a unique perspective on life that challenges me and helps me grow.", 20)
+        "I appreciate how you always take the time to listen to me and understand my feelings, even when I'm being irrational.", 20)
 
 
 def create_self_sample_emotional_letters(user, sample_user):
